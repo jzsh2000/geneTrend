5e66e1ae39230b12a913b7e0dfc59b8f7cb19cae
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2017-01-27 16:01:27 +0800

    Search pubmed according to entrez gene id

---
+++ script/linkGeneToPubmed.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+parallel mkdir -p data/pubmed/{} ::: $(seq 0 9)
+parallel --jobs 1 \
+    "esearch -db gene -query '{}[uid]' \
+    | elink -target pubmed \
+    | efetch -format uid \
+    | sort -n > data/pubmed/{= s/.*(?=.)// =}/{}.pmid" \
+    :::: data/geneList.txt
