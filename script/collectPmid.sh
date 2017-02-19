9960871f1ff709689a9616c93d3999da91416d65
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2017-02-19 19:45:53 +0800

    Fix find command

--- script/collectPmid.sh
+++ script/collectPmid.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 txid=${1:-9606}
-find data/$txid/pubmed/?/*.pmid -exec cat {} + \
+find data/$txid/pubmed/? -name '*.pmid' -exec cat {} + \
     | sort -nu > data/$txid/pubmed/gene.pmid
